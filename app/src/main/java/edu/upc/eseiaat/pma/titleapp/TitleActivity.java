cf8307822108539b3ec1c0ec1c5994de7c137d29
andreaortega <andreaortegadumont@gmail.com>
2017-10-16 15:38:20 +0200

    2)Obrim l'activitat EditTitleActivity pero encara no funciona

--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
@@ -1,7 +1,10 @@
 package edu.upc.eseiaat.pma.titleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ViewUtils;
+import android.view.View;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+    }
+
+    public void editTitle (View view){
+        //1. Crear un 'intent'
+        Intent intent= new Intent (this,EditTitleActivity.class);
+        //Passar l'intent a Android perque obri l'activitat
+        startActivity(intent);
     }
 }
