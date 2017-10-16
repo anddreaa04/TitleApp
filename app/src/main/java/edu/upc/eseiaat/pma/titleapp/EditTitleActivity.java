e60742e4d7a3ed0e43c099aaf2ce6cdb1fd18693
andreaortega <andreaortegadumont@gmail.com>
2017-10-16 16:10:58 +0200

    el titol antic es substitueix pel nou correctament

--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/EditTitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/EditTitleActivity.java
@@ -3,6 +3,7 @@ package edu.upc.eseiaat.pma.titleapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class EditTitleActivity extends AppCompatActivity {
@@ -18,5 +19,12 @@ public class EditTitleActivity extends AppCompatActivity {
         edit_title= (EditText)findViewById(R.id.edit_title);
         edit_title.setText(title);
 
+    }
+    public void saveTitle (View view){
+        String new_title= edit_title.getText().toString();
+        Intent data = new Intent();
+        data.putExtra("title", new_title);
+        setResult(RESULT_OK,data);
+        finish();
     }
 }
--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/TitleActivity.java
@@ -10,12 +10,15 @@ import android.widget.TextView;
 public class TitleActivity extends AppCompatActivity {
 
     private String title= "Aquest es el titol inicial...";
+    private TextView title_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
-        TextView title_text= (TextView) findViewById(R.id.title);
+        title_text=(TextView)findViewById(R.id.title);
+        title_text.setText(title);
+
     }
 
     public void editTitle (View view){
@@ -24,6 +27,19 @@ public class TitleActivity extends AppCompatActivity {
        //2. Afegir parametres (dades extra) a la crida de l'activitat
         intent.putExtra("title",title);
         //3.Passar l'intent a Android perque obri l'activitat
-        startActivity(intent);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+      switch(requestCode){
+          case 0:
+              if (resultCode== AppCompatActivity.RESULT_OK){
+                  String new_title= data.getStringExtra("title");
+                  title=new_title;
+                  title_text.setText(title);
+              }
+      }
+
     }
 }
